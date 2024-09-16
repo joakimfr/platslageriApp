5dfcd70a0b14ecd402ab75a9e134ff89b519b765
joakimfr <fritzjoakim@hotmail.com>
2024-09-16 22:38:10 +0200

    started styling project screen

--- app/(tabs)/project/index.tsx
+++ app/(tabs)/project/index.tsx
@@ -1,17 +1,53 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
-import { useRouter } from 'expo-router';
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { useRouter } from "expo-router";
 
 export default function ProjectScreen() {
   const router = useRouter();
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Projekt-fliken</Text>
-      {/* Navigera till Skapa projekt */}
-      <Button title="Skapa projekt" onPress={() => router.push('/(tabs)/project/createProject')} />
-      {/* Navigera till Visa nuvarande projekt */}
-      <Button title="Visa nuvarande projekt" onPress={() => router.push('/(tabs)/project/currentProjects')} />
+    <View style={styles.container}>
+      <Text style={styles.title}>Projekt</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => router.push("/(tabs)/project/currentProjects")}
+      >
+        <Text style={styles.buttonText}>Dina projekt</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => router.push("/(tabs)/project/createProject")}
+      >
+        <Text style={styles.buttonText}>Skapa projekt</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FF7F50",
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "#FFFFFF",
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "#2C3E50",
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+    width: 195,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "#FFFFFF",
+    fontSize: 18,
+  },
+});
