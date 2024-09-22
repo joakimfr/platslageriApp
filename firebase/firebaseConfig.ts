23645409e10d05be916e226fcfc15eecc58ddfb3
joakimfr <fritzjoakim@hotmail.com>
2024-09-22 21:16:00 +0200

    started to implement firbase

---
+++ firebase/firebaseConfig.ts
@@ -0,0 +1,19 @@
+// firebase/firebaseConfig.ts
+
+import { initializeApp } from "firebase/app";
+
+// Din Firebase-konfiguration
+const firebaseConfig = {
+  apiKey: "AIzaSyDv1y7DEyMXN1NbkLqafecYPRZ60TVgUUk",
+  authDomain: "projektapp-d15b0.firebaseapp.com",
+  projectId: "projektapp-d15b0",
+  storageBucket: "projektapp-d15b0.appspot.com",
+  messagingSenderId: "333781587236",
+  appId: "1:333781587236:web:e902b732379487a94e4fb1",
+  measurementId: "G-30C2BCXHLT"
+};
+
+// Initialisera Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
